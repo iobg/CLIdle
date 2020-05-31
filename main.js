9272e41ab16d3f99665fbeb621a89f46dcd7acf0
Banks <iobg0320@gmail.com>
2020-05-31 18:59:15 -0400

    added passive income, hold limiter(kinda gross) among other things

--- main.js
+++ main.js
@@ -8,23 +8,46 @@ process.stdin.resume();
 keypress(process.stdin);
 
 const gameState = {
-	count: 0
+	count: 0,
+	enterHeld: false,
+	enterPower: 1,
+	passiveInc: 0.1
 }
 
 process.stdin.on('keypress', function (ch, key) {
   if (key && key.ctrl && key.name == 'c') {
     process.stdin.pause();
+    process.exit();
   }
 
   if (key && key.name == 'return') {
-   	render(<ResourceCount count={++gameState.count}/>)
+   	handlePress()
   }
 
 });
 
+const handlePress = () => {
+	//node can only check for keypress so... keep it from incrementing super fast when held
+	//flag gets cleared in main loop... maybe upgrades make it faster?
+	if(!gameState.enterHeld)
+		gameState.count += gameState.enterPower;
+	render(<ResourceCount count={gameState.count.toFixed(2)}/>)
+	gameState.enterHeld = true;
+}
+
+const update = () => {
+	gameState.enterHeld = false;
+	gameState.count += gameState.passiveInc;
+	render(<ResourceCount count={gameState.count.toFixed(2)}/>)
+}
 
 const init = ()=>{
+	console.clear()
 	render(<ResourceCount count={0}/>)
 }
+
+const mainLoop = setInterval(()=>{
+	update()
+}, 100)
 
 init();
